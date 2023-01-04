83226ebd6449078866bb3a44c5ba3bc46616fb04
Marcelo Alcocer <marcelo.j.p.alcocer@gmail.com>
2023-01-04 21:58:50 +0100

    Add custom CSS

--- source/conf.py
+++ source/conf.py
@@ -15,13 +15,11 @@ author = "Marcelo Alcocer"
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
